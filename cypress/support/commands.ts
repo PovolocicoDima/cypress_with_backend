c5d998edd42156542f60463cf67518299c53af07
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2023-11-22 17:41:21 +0200

    added eslint types for cypress and prettier for ts code

---
+++ cypress/support/commands.ts
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+export {}
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+Cypress.Commands.add('loginToApplication', () => {
+    cy.visit('/login')
+    cy.get('[placeholder="Email"]').clear().type('dima@test.com')
+    cy.get('[placeholder="Password"]').clear().type('cypresstest')
+    cy.get('form').submit()
+})
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            loginToApplication(): Chainable<void>
+        }
+    }
+}
